afdc78b2a6ee7194c439ed08cef77b1f9337c445
Siddharth Srinivasan <sshrinivasan@gmail.com>
2015-05-07 17:02:45 -0700

    Minor fixes

--- pollutantMean.r
+++ pollutantMean.r
@@ -1,23 +1,30 @@
 compute <- function(filename, pollutant) {
-  # given a csv filename, return the average pollutant
-  # columns are 'sulfate', 'nitrate', 'ID'
+  ### given a csv filename, return the average pollutant
+  # columns are 'sulfate', 'nitrate', 'ID' ###
+  
   df = read.csv(filename)
   
   # New DataFrame where only valid rows are present
   valid_df = df[!is.na(df[pollutant]),]
+  
+  # If the resutant is not a null dataframe, there is at least one valid row
   if (nrow(valid_df) > 0) {
-  # Mean of the selected column
-  total = sum(valid_df[pollutant])
-  len = nrow(valid_df)
-  return(c(total,len))
+    # Mean of the selected column
+    total = sum(valid_df[pollutant])
+    len = nrow(valid_df)
+    return(c(total,len))
   }
+  # If empty dataframe, then return 0
   else {
-    return(c(0,1))
+    return(c(0,0))
   }
 }
 
+## AVERAGE POLLUTANTS
 pollutantmean <- function(directory, pollutant, id=1:332) {
-  # Convert the integer ID to three digit filename: 001.csv
+  ### Convert the integer ID to three digit filename: 001.csv ###
+  
+  # By default, take all files
   c <- sprintf("%03d", id)
   csv_files = paste(directory, "/", c,".csv", sep = "")
   
@@ -31,5 +38,6 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
      total_count = total_count + site_values[2]
   }
   avg_pollutant = total_pollutant / total_count
-  print(avg_pollutant)
+  return(avg_pollutant)
 }
+
